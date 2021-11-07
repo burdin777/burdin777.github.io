494db590fc2af5161b29bb8e6e83a7011258420f
burdin777 <88722210+burdin777@users.noreply.github.com>
2021-11-07 11:46:34 +0200

    Add files via upload

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -32,11 +32,7 @@ $('a[href^="#"]').click(function(){
 });
 
     
-    
-function showEditor() {
-    $("#EditModal").modal("show");//показываем модальное окно
-    $("#EditModal").appendTo("body");//перемещаем по DOM в конец body
-}
+
     
     
     
